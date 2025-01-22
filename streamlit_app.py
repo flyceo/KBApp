fa15bca0ffc4fc0ae721075b7f451811e089b07a
Roland <28514022+flyceo@users.noreply.github.com>
2025-01-22 19:51:46 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,4 +1,31 @@
+from pathlib import Path
+import pandas as pd
+import numpy as np
 import streamlit as st
+
+datei = "fz6_2024.xls"
+blatt = "FZ 6.1"
+kopfzeile = 7
+herstellerspalte = "Herstellerklartext"
+typspalte = "Handelsname"
+hsnspalte = "Hersteller-\nschlüssel-\nnummer"
+tsnspalte = "Typ-\nschlüssel-\nnummer"
+wertspalte = "Anzahl"
+
+if Path(datei.split(".")[0] + ".pkl").exists():
+    pd.read_pickle(datei.split(".")[0] + ".pkl")
+else:
+    df = pd.read_excel(datei, sheet_name=blatt, header=kopfzeile)
+    df = df.drop(df.columns[[0]], axis=1)
+    df = df[~df[wertspalte].isna()]
+    df = df.fillna("");
+    df[wertspalte] = df[wertspalte].astype("int32")
+    df[herstellerspalte] = df[herstellerspalte].astype("category")
+    df[hsnspalte] = df[hsnspalte].astype("category")
+    df[tsnspalte] = df[tsnspalte].astype("category")
+    df.to_pickle(datei.split(".")[0] + ".pkl")
+
+
 
 st.write("""
 # My first app
