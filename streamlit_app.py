ed089baba78d77d4e40174e65addce378c35913a
Roland <28514022+flyceo@users.noreply.github.com>
2025-01-23 00:46:04 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -82,7 +82,7 @@ dff = df[df["Typ"].str.contains("(?i)" + typsuche) & df["Hersteller"].str.contai
 & df["HSN"].str.contains("(?i)" + hsnsuche) & df["TSN"].str.contains("(?i)" + tsnsuche)]
 
 st.dataframe(dff, use_container_width=True, hide_index=True)
-st.write("**" + str(len(dff.index)) + "** Datensätze gefunden, Gesamtsumme **" + str(df["Anzahl"].sum()) + "**)
+st.write("**" + str(len(dff.index)) + "** Datensätze gefunden, Gesamtsumme **" + str(df["Anzahl"].sum()) + "**")
 st.write("Datenquelle: Kraftfahrt-Bundesamt, Bestand nach Herstellern und Typen (FZ 6), " + dt.date.today().strftime("%d.%m.%Y") + "; [Datenlizenz by-2-0](https://www.govdata.de/dl-de/by-2-0); eigene Darstellung")
 
 
