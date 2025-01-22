a58cd82c9c1fe66488e61a79b0afad802a2dfc35
Roland <28514022+flyceo@users.noreply.github.com>
2025-01-22 21:35:42 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -48,6 +48,6 @@ dff = df[df["Typ"].str.contains("(?i)" + typsuche) & df["Hersteller"].str.contai
 & df["HSN"].str.contains("(?i)" + hsnsuche) & df["TSN"].str.contains("(?i)" + tsnsuche)]
 
 st.dataframe(dff, use_container_width=True, hide_index=True)
-##st.write(len(dff.index) + " Daten gefunden")
+st.write("**" + str(len(dff.index)) + "** Daten gefunden")
 
 
